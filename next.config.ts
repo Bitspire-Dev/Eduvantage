7f69c07ebcd541c34ed6bacfa18b725687ec17d6
Jakub Pujanek <48909@office.upsl.edu.pl>
2026-08-09 16:29:35 +0200

    refaktor duży, wydzielenie componentów na hooki i lib, optymalizacja wydajności

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'dist',
+  distDir: '.next',
   // Export as directories with index.html (e.g. /cookies/index.html)
   trailingSlash: true,
   images: {
